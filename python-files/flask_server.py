d448788abd46907f4e8d27cf6c60064cb08f6b16
mohammad1774 <mohammad.ahmed1774@gmail.com>
2024-03-15 03:36:53 +0530

    capstone project

--- python-files/flask_server.py
+++ python-files/flask_server.py
@@ -1,45 +1,47 @@
-# Example: Flask app for image data in a file named app.py
 from flask import Flask, jsonify, request
-from flask_ngrok import run_with_ngrok
-from tensorflow import argmax
 from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing import image
 import numpy as np
+import subprocess
+import requests
+import time
 
-img_size = [255,255]
+
 app = Flask(__name__)
-run_with_ngrok(app)  # Start ngrok when app is run
 
-# Load the trained model
-model_path = 'D:\\project-forest\\MyDrive\\capstone-project\\training_log_custom\\trained_model\\hdf5_model\\trained_model_custom.h5'
+# Load the pre-trained TensorFlow model
+model_path = 'C:\\Users\\Mohammad\\Documents\\GitHub\\project-forest-fire\\MyDrive\\capstone-project\\training_log_custom\\trained_model\\hdf5_model\\trained_model_custom.h5'
+
+model = load_model(model_path)
 
 def preprocess_image(img_path):
-    # Load and preprocess the image
-    img = image.load_img(img_path, target_size=img_size)
+    img = image.load_img(img_path, target_size=(255, 255))
     img_array = image.img_to_array(img)
     img_array = np.expand_dims(img_array, axis=0)
-    img_array /= 255.0  # Normalize the pixel values
+    img_array /= 255.0
     return img_array
 
 @app.route('/predict', methods=['POST'])
 def predict():
     try:
-        # Get the image file from the request
         img_file = request.files['image']
-        # Save the image to a temporary file
-        img_path = "/tmp/temp_image.jpg"
+        img_path = "temp_image.jpg"
         img_file.save(img_path)
-        # Preprocess the image
         input_data = preprocess_image(img_path)
-        # Make predictions
         predictions = model.predict(input_data)
-        
-        predictions = argmax(predictions)
-
-        # Return predictions as JSON
-        return jsonify(predictions.tolist())
+        predicted_class = np.argmax(predictions, axis=1)
+        return jsonify({'predicted_class': int(predicted_class), 'predictions': predictions.tolist()})
     except Exception as e:
         return jsonify({"error": str(e)})
 
 if __name__ == '__main__':
+    ngrok_api = subprocess.Popen(['C:\\Users\\Mohammad\\Documents\\GitHub\\project-forest-fire\\python-files\\ngrok.exe','http','5000'])
+
+    time.sleep(5)
+    ngrok_api_url = requests.get('http://localhost:4040/api/tunnels').json()['tunnels'][0]['public_url']
+
+    print("Ngrok URL:", ngrok_api_url)
+
+    with open('url.txt','w') as file:
+        file.writelines(ngrok_api_url)
     app.run()
